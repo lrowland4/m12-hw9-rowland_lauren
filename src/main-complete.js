77d6049d8bc9f58de7b5c35daa97a1a5c79f145f
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-11-09 07:52:27 -0500

    Turn Nav red when clicked

--- src/main-complete.js
+++ src/main-complete.js
@@ -1,6 +1,14 @@
+
 const navButtons = document.querySelectorAll('#navbar ul li');
+const navBar = document.querySelector('#navbar');
 
 function wrapper() {
+
+  const changeColor = () => {
+  	navBar.classList.toggle('red');
+
+  };
+
   const loopThrough = () => {
 
   	let thisButtonText = this.childNodes[0].nodeValue;
@@ -8,7 +16,7 @@ function wrapper() {
 
 
     for (let i = 0; i < thisButtonLength; i++) {
-    	console.log('hi');
+    	changeColor();
 
     }
 
@@ -20,3 +28,4 @@ for(let i = 0; i < navButtons.length; i++) {
 	navButtons[i].addEventListener('click', wrapper);
 
 };
+
